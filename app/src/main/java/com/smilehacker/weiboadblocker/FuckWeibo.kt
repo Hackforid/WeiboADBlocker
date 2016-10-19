18e68213dd743455afe0a1fdba8bdb887cdf6a01
Sai <zhouquanbest@gmail.com>
2016-10-19 15:55:15 +0800

    fix bug add hot loader

--- app/src/main/java/com/smilehacker/weiboadblocker/FuckWeibo.kt
+++ app/src/main/java/com/smilehacker/weiboadblocker/FuckWeibo.kt
@@ -1,6 +1,5 @@
 package com.smilehacker.weiboadblocker
 
-import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedHelpers
@@ -12,91 +11,100 @@ import java.util.*
 /**
  * Created by kleist on 2016/10/12.
  */
-class FuckWeibo : IXposedHookLoadPackage {
+class FuckWeibo {
 
 
     private val DESC_CLASS = "com.sina.weibo.models.MBlogListBaseObject"
     private val HTTP_CLASS = "java.net.HttpURLConnection"
 
     private var mLastStatus : ArrayList<Any?>? = null
-    private var mLastFixedStatus : ArrayList<Any?>? = null
 
-    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam?) {
-        //XposedBridge.log("load app: ${lpparam?.packageName}")
-        if (lpparam!!.packageName!!.indexOf("com.sina.weibo") < 0) {
-            return
-        }
-
-        XposedBridge.log("try fuck sina weibo ${lpparam?.packageName}")
-        fuckWeibo(lpparam!!)
-    }
+    private val BLOCK_HOST_LIST = arrayOf("sdkapp.mobile.sina.cn", "adashx.m.taobao.com", "adashbc.m.taobao.com")
 
 
     fun fuckWeibo(lpparam: XC_LoadPackage.LoadPackageParam) {
 
-        findAndHookMethod(DESC_CLASS, lpparam.classLoader, "getTrends", object : XC_MethodHook() {
-            override fun beforeHookedMethod(param: MethodHookParam?) {
-                param?.result = Collections.EMPTY_LIST
-            }
-        })
-        findAndHookMethod(DESC_CLASS, lpparam.classLoader, "setTrends", List::class.java, object : XC_MethodHook() {
-            override fun beforeHookedMethod(param: MethodHookParam?) {
-                param?.result = null
-            }
-        })
-        findAndHookMethod(DESC_CLASS, lpparam.classLoader, "insetTrend", object : XC_MethodHook() {
-            override fun beforeHookedMethod(param: MethodHookParam?) {
-                param?.result = Collections.EMPTY_LIST
-            }
-        })
+        XposedBridge.log("weiboADblocker version 3")
+
+//        findAndHookMethod(DESC_CLASS, lpparam.classLoader, "getTrends", object : XC_MethodHook() {
+//            override fun beforeHookedMethod(param: MethodHookParam?) {
+//                XposedBridge.log("getTrends")
+//                param?.result = Collections.EMPTY_LIST
+//            }
+//        })
+//        findAndHookMethod(DESC_CLASS, lpparam.classLoader, "setTrends", List::class.java, object : XC_MethodHook() {
+//            override fun beforeHookedMethod(param: MethodHookParam?) {
+//                XposedBridge.log("setTrends")
+//                param?.result = null
+//            }
+//        })
+//        findAndHookMethod(DESC_CLASS, lpparam.classLoader, "insetTrend", object : XC_MethodHook() {
+//            override fun beforeHookedMethod(param: MethodHookParam?) {
+//                XposedBridge.log("insetTrends")
+//                param?.result = Collections.EMPTY_LIST
+//            }
+//        })
 
         findAndHookMethod(DESC_CLASS, lpparam.classLoader, "setStatuses", List::class.java, object : XC_MethodHook() {
-            override fun beforeHookedMethod(param: MethodHookParam?) {
-                val statuses = param!!.args[0] as List<Any?>
-                val fixStatus = ArrayList<Any?>()
-                statuses.forEach {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                XposedBridge.log("setStatuses")
+                val statuses = param.args[0] as ArrayList<Any?>
+
+                val iterator = statuses.iterator()
+                while (iterator.hasNext()) {
+                    val it = iterator.next()
                     val value = XposedHelpers.getObjectField(it, "mblogtypename") as String
 
                     if (!value.isNullOrBlank()) {
-                        XposedBridge.log("setStatuses remove ad: $value")
-                    } else {
-                        fixStatus.add(it)
+                        if (BuildConfig.DEBUG) {
+                            XposedBridge.log("setStatuses remove ad: $value")
+                        }
+                        iterator.remove()
                     }
                 }
-                param.args[0] = fixStatus
+
+                param.args[0] = statuses
             }
         })
+
         findAndHookMethod(DESC_CLASS, lpparam.classLoader, "getStatuses", object : XC_MethodHook() {
-            override fun afterHookedMethod(param: MethodHookParam?) {
-                val statuses = param!!.result as ArrayList<Any?>
-                if (statuses == mLastStatus) {
-                    param.result = mLastFixedStatus
+            override fun afterHookedMethod(param: MethodHookParam) {
+                val statuses = param.result as ArrayList<Any?>
+
+                XposedBridge.log("getStatuses: size=" + statuses.size)
+
+                if (statuses.equals(mLastStatus)) {
                     return
                 }
 
-                val fixStatus = ArrayList<Any?>()
-                statuses.forEach {
+                val iterator = statuses.iterator()
+                while (iterator.hasNext()) {
+                    val it = iterator.next()
                     val value = XposedHelpers.getObjectField(it, "mblogtypename") as String
 
                     if (!value.isNullOrBlank()) {
-                        XposedBridge.log("getStatuses remove ad: $value")
-                    } else {
-                        fixStatus.add(it)
+                        if (BuildConfig.DEBUG) {
+                            XposedBridge.log("getStatuses remove ad: $value")
+                        }
+                        iterator.remove()
                     }
                 }
-                mLastFixedStatus = fixStatus
+
                 mLastStatus = statuses
-                param.result = fixStatus
+                param.result = statuses
             }
         })
 
         findAndHookMethod("java.net.URL", lpparam.classLoader, "openConnection", object : XC_MethodHook() {
             override fun beforeHookedMethod(param: MethodHookParam?) {
-                XposedBridge.log("URL openConnection")
                 val url = param!!.thisObject as URL
-                if (url.toString().indexOf("sdkapp.mobile.sina.cn") >= 0) {
-                    XposedBridge.log("find ad url")
-                    param.result = null
+                BLOCK_HOST_LIST.forEach {
+                    if (url.toString().indexOf(it) >= 0) {
+                        if (BuildConfig.DEBUG) {
+                            XposedBridge.log("block ad url $url")
+                        }
+                        param.result = null
+                    }
                 }
             }
         })
---
+++ app/src/main/java/com/smilehacker/weiboadblocker/Main.kt
@@ -0,0 +1,52 @@
+package com.smilehacker.weiboadblocker
+
+import dalvik.system.PathClassLoader
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+import java.io.File
+
+/**
+ * Created by kleist on 2016/10/14.
+ */
+class Main: IXposedHookLoadPackage {
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam?) {
+        if (lpparam!!.packageName!!.indexOf("com.sina.weibo") < 0) {
+            return
+        }
+        XposedBridge.log("try f**k sina weibo ${lpparam?.packageName}")
+
+        try {
+            if (BuildConfig.DEBUG) {
+                XposedBridge.log("run in hot loader")
+                runInDebugHotloader(lpparam)
+            } else {
+                hook(lpparam)
+            }
+        } catch (e: Throwable) {
+            XposedBridge.log(e)
+        }
+    }
+
+    fun runInDebugHotloader(lpparam: XC_LoadPackage.LoadPackageParam) {
+
+        val packageName = Main::class.java.`package`.name
+        var filePath = "/data/app/$packageName-1/base.apk"
+        if (!File(filePath).exists()) {
+            filePath = "/data/app/$packageName-2/base.apk"
+            if (!File(filePath).exists()) {
+                XposedBridge.log("Error:在/data/app找不到APK文件" + filePath)
+                return
+            }
+        }
+        val pathClassLoader = PathClassLoader(filePath, ClassLoader.getSystemClassLoader())
+        val aClass = Class.forName(packageName + "." + Main::class.java.simpleName, true, pathClassLoader)
+        val aClassMethod = aClass.getMethod("hook", XC_LoadPackage.LoadPackageParam::class.java)
+        aClassMethod.invoke(aClass.newInstance(), lpparam)
+    }
+
+    fun hook(lpparam: XC_LoadPackage.LoadPackageParam) {
+        FuckWeibo().fuckWeibo(lpparam)
+    }
+}
+
